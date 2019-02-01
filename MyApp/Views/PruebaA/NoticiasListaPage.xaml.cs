ef1e36d5d5af68997b10864cb2e6b4bb386afe2f
jucaripo <jucaripo@gmail.com>
2019-02-01 13:54:32 -0600

    Pagina Base de Noticias

---
+++ MyApp/Views/PruebaA/NoticiasListaPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MyApp.Views.PruebaA
+{
+    public partial class NoticiasListaPage : ContentPage
+    {
+        public NoticiasListaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
